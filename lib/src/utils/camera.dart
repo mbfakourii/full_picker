7d72f23a4dfd6242f94df1a3f869c17fe9cca0fa
mbfakourii <mbfakourii@gmail.com>
2022-09-10 16:40:26 +0430

    fix Permission error in camera

--- lib/src/utils/camera.dart
+++ lib/src/utils/camera.dart
@@ -11,11 +11,7 @@ class Camera extends StatefulWidget {
   final bool imageCamera;
   final String prefixName;
 
-  const Camera(
-      {Key? key,
-      required this.imageCamera,
-      required this.videoCamera,
-      required this.prefixName})
+  const Camera({Key? key, required this.imageCamera, required this.videoCamera, required this.prefixName})
       : super(key: key);
 
   @override
@@ -100,16 +96,13 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
   // Main Widget for Camera
   Widget _cameraPreviewWidget() {
     if (controller == null || !controller!.value.isInitialized) {
-      onNewCameraSelected(cameras.firstWhere((description) =>
-          description.lensDirection == CameraLensDirection.back));
+      onNewCameraSelected(cameras.firstWhere((description) => description.lensDirection == CameraLensDirection.back));
     }
 
     // Set aspectRatio Camera
     double scale;
     try {
-      scale = 1 /
-          (controller!.value.aspectRatio *
-              MediaQuery.of(context).size.aspectRatio);
+      scale = 1 / (controller!.value.aspectRatio * MediaQuery.of(context).size.aspectRatio);
     } catch (e) {
       scale = 1.0;
     }
@@ -145,10 +138,8 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
     takePicture().then((String? filePath) {
       if (filePath == "") return;
       if (mounted) {
-        Navigator.pop(
-            context,
-            OutputFile([File(filePath!).readAsBytesSync()],
-                FilePickerType.image, ["${widget.prefixName}.jpg"]));
+        Navigator.pop(context,
+            OutputFile([File(filePath!).readAsBytesSync()], FilePickerType.image, ["${widget.prefixName}.jpg"]));
       }
     });
   }
@@ -158,10 +149,8 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
     stopVideoClick = true;
     stopVideoRecording().then((file) {
       if (mounted) {
-        Navigator.pop(
-            context,
-            OutputFile([File(file!.path).readAsBytesSync()],
-                FilePickerType.video, ["${widget.prefixName}.mp4"]));
+        Navigator.pop(context,
+            OutputFile([File(file!.path).readAsBytesSync()], FilePickerType.video, ["${widget.prefixName}.mp4"]));
       }
     });
   }
@@ -220,13 +209,12 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
 
   // show Camera Exception
   void _showCameraException(CameraException e) {
-    if (e.code == "cameraPermission") {
-      Navigator.pop(context);
-      Navigator.pop(context, 1);
+    if (e.code == "cameraPermission" || e.code == "CameraAccessDenied") {
+      if (mounted) {
+        Navigator.pop(context);
+      }
 
-      Fluttertoast.showToast(
-          msg: globalLanguage.denyAccessPermission,
-          toastLength: Toast.LENGTH_SHORT);
+      Fluttertoast.showToast(msg: globalLanguage.denyAccessPermission, toastLength: Toast.LENGTH_SHORT);
     }
   }
 
@@ -266,8 +254,7 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
             maintainSize: true,
             maintainAnimation: true,
             maintainState: true,
-            visible: (widget.imageCamera && widget.videoCamera) &&
-                toggleCameraAndTextVisibility,
+            visible: (widget.imageCamera && widget.videoCamera) && toggleCameraAndTextVisibility,
             child: Text(globalLanguage.tapForPhotoHoldForVideo,
                 style: const TextStyle(color: Color(0xa3ffffff), fontSize: 20)),
           ),
@@ -309,11 +296,9 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
   void changeCamera() {
     if (firstCamera) {
       firstCamera = false;
-      onNewCameraSelected(cameras.firstWhere((description) =>
-          description.lensDirection == CameraLensDirection.front));
+      onNewCameraSelected(cameras.firstWhere((description) => description.lensDirection == CameraLensDirection.front));
     } else {
-      onNewCameraSelected(cameras.firstWhere((description) =>
-          description.lensDirection == CameraLensDirection.back));
+      onNewCameraSelected(cameras.firstWhere((description) => description.lensDirection == CameraLensDirection.back));
       firstCamera = true;
     }
   }
