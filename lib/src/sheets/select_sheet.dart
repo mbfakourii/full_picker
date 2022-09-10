e92c4c95babdcb27af1e0f76e2dc8029861945a0
mbfakourii <mbfakourii@gmail.com>
2022-09-10 18:24:26 +0430

    fix close sheet in multi platform android web windows

--- lib/src/sheets/select_sheet.dart
+++ lib/src/sheets/select_sheet.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../full_picker.dart';
 
@@ -70,38 +69,43 @@ class _SheetSelectState extends State<SelectSheet> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: const BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisSize: MainAxisSize.min, children: [
-          topSheet(globalLanguage.selectFile, context),
-          GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height / 2),
-              ),
-              itemCount: itemList.length,
-              itemBuilder: (context, index) {
-                return Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                      customBorder: const CircleBorder(),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(itemList[index].icon, size: 30),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: Text(itemList[index].name),
-                          )
-                        ],
-                      ),
-                      onTap: () {
-                        goPage(itemList[index]);
-                      }),
-                );
-              })
-        ]));
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              topSheet(globalLanguage.selectFile, context),
+              GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: MediaQuery.of(context).size.width /
+                        (MediaQuery.of(context).size.height / 2),
+                  ),
+                  itemCount: itemList.length,
+                  itemBuilder: (context, index) {
+                    return Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                          customBorder: const CircleBorder(),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(itemList[index].icon, size: 30),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text(itemList[index].name),
+                              )
+                            ],
+                          ),
+                          onTap: () {
+                            goPage(itemList[index]);
+                          }),
+                    );
+                  })
+            ]));
   }
 
   // show file picker
