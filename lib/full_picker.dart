811d4f110c2abbcbc29033190d38752045fe65d8
mbfakourii <mbfakourii@gmail.com>
2023-09-27 14:42:27 +0330

    Fixed bugs

--- lib/full_picker.dart
+++ lib/full_picker.dart
@@ -4,5 +4,5 @@ export 'src/dialogs/progress_indicator_dialog.dart';
 export 'src/sheets/select_sheet.dart';
 export 'src/utils/camera.dart';
 export 'src/utils/common_utils.dart';
-export 'src/utils/language.dart';
+export 'src/utils/full_picker_language.dart';
 export 'src/widgets/full_picker.dart';
