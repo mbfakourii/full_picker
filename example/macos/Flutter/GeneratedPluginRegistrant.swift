d14e2ac037a8c4f01048d0a1513da0deaf7d1afb
mbfakourii <mbfakourii@gmail.com>
2025-07-26 09:12:00 +0330

    feat: update record package to version 6.0.0 and refactor plugin registration

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,10 @@ import Foundation
 
 import light_compressor
 import path_provider_foundation
-import record_darwin
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LightCompressorPlugin.register(with: registry.registrar(forPlugin: "LightCompressorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
 }
